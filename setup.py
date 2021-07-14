e54d346778731e8265e55c331a3beb3d0b79d53d
pullegaddi <prashullegaddi@gmail.com>
2021-07-14 13:01:06 +0530

    update

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "."},
+    package_dir={"": "examples"},
     packages=setuptools.find_packages(where="examples"),
     python_requires=">=3.6",
     install_requires=[

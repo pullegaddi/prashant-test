9e859ab049550fdb197c86c70dad2fd9b144b503
pullegaddi <prashullegaddi@gmail.com>
2021-07-14 15:28:56 +0530

    Changing example code

--- examples1/tests/test_example.py
+++
@@ -1,7 +0,0 @@
-from examples1 import example
-import unittest
-
-
-class ExampleMethods(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(example.get_hello(), "hello, world!")
--- setup.py
+++ setup.py
@@ -4,26 +4,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-prashant-ullegaddi",
+    name="example-pkg-ai-utils",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "examples1"},
-    packages=setuptools.find_packages(where="examples1"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=[
-        "PyYAML==5.1"
-        ]
 )
